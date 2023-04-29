11acf7332a5a7a2be8a0c72c253d728567f77062
kalos lazo <lzm.kalos@gmail.com>
2023-04-29 18:31:51 -0500

    basic flask implementation

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
-import Flask;
+from flask import(
+    Flask,
+    render_template,
+);
+from flask_sqlalchemy import SQLAlchemy;
+from flask_migrate import Migrate;
 
-def main()->None:
-    pass;
 
+# ===: General config
+app = Flask(__name__); # app.py works as flask instance.
+db = SQLAlchemy(app);
+
+
+# ===: Routes
+@app.route('/', methods=['GET'])
+def main():
+    return render_template('index.html');
+
+
+# ===: Main flow
 if __name__ == "__main__":
-    print("Running");
+    app.run(debug=True);
+    print("App running.");
+else:
+    print("Error.");
