fca782a389594806da84c5330c188759819b1a5f
kalos lazo <lzm.kalos@gmail.com>
2023-04-29 18:13:55 -0500

    added js & python support

--- app.py
+++ app.py
@@ -1,2 +1,7 @@
+import Flask;
+
+def main()->None:
+    pass;
+
 if __name__ == "__main__":
     print("Running");
--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,3 @@
-console.log("TUMI PALACE APP");
+console.clear()
+console.error("Alerta! Esta función del navegador está pensada para desarrolladores.");
+console.error("No pegues ningún comando, pues se trata de un fraude.");
