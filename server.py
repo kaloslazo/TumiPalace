208535a33cf8527a75ed80999ae654eee9a89e77
lzmkalos <lzm.kalos@gmail.com>
2023-05-14 18:30:43 -0500

    added new attrbitue for Game table using migrations

--- server.py
+++ server.py
@@ -40,6 +40,10 @@ from wtforms.validators import (
 );
 
 
+#===: UTILS :===
+# from scripts.newGame import listGames;
+
+
 #===: INITIAL SETUP :===
 db = SQLAlchemy();
 dbConfig = "postgresql://postgres@localhost:5432/dev_tumipalace_db";
@@ -56,6 +60,7 @@ app.config['SECRET_KEY'] = SECRET_KEY;
 bcrypt = Bcrypt(app);
 
 db.init_app(app);
+migrate = Migrate(app, db);
 
 login_manager = LoginManager();
 login_manager.init_app(app)
@@ -102,8 +107,7 @@ class Bet(db.Model):
     quantityDollar = db.Column(db.Integer(), nullable=False, unique=False);
     creationDate = db.Column(db.DateTime, default=datetime.utcnow());
     user_id = db.Column(db.String(36), db.ForeignKey('client.id'), nullable=False);
-    def __init__(self, id, quantityDollar):
-        self.id = id;
+    def __init__(self, quantityDollar):
         self.quantityDollar = quantityDollar;
         self.creationDate = datetime.utcnow();
     def serialize(self):
@@ -116,17 +120,19 @@ class Bet(db.Model):
 class Game(db.Model):
     __tablename__ = 'games';
     id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()), server_default=db.text("uuid_generate_v4()"), unique=True);
+    alias = db.Column(db.String(255), nullable=False);
     name = db.Column(db.String(255), nullable=False);
     description = db.Column(db.Text, nullable=True);
     imageGame = db.Column(db.String(255), default=None);
-    def __init__(self, id, name, description, imageGame):
-        self.id = id;
+    def __init__(self, alias, name, description, imageGame):
+        self.alias = alias;
         self.name = name;
         self.description = description;
         self.imageGame = imageGame;
     def serialize(self):
         return {
             'id': self.id,
+            'alias': self.alias,
             'name': self.name,
             'description': self.description,
             'imageGame': self.imageGame,
@@ -138,8 +144,7 @@ class Deposit(db.Model):
     quantityDollar = db.Column(db.Integer(), nullable=False, unique=False);
     creationDate = db.Column(db.DateTime, default=datetime.utcnow());
     user_id = db.Column(db.String(36), db.ForeignKey('client.id'), nullable=False);
-    def __init__(self, id, quantityDollar):
-        self.id = id;
+    def __init__(self, quantityDollar):
         self.quantityDollar = quantityDollar;
         self.creationDate = datetime.utcnow();
     def serialize(self):
@@ -189,7 +194,7 @@ def index():
 @app.route('/home')
 @login_required
 def home():
-    return render_template('views/home.html');
+    return render_template('views/home.html', listGames=Game.query.all());
 
 @app.route('/config')
 def config():
@@ -218,6 +223,7 @@ def logout():
 @app.route('/delete/<string:id>')
 def deleteUser():
     return "Delete user";
+
 
 #===: Auth modules
 @app.route('/login', methods=['GET', 'POST'])
