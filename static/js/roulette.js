db94461a68d19a7cd72a3c9208f29b6098b325e2
Giancarlo Humberto Ferreyra Uribe <giancarlohumberto@gmail.com>
2023-05-16 14:12:36 -0500

    Adding Premio.mp3

--- static/js/roulette.js
+++ static/js/roulette.js
@@ -3,33 +3,33 @@ console.log("===: TumiPalace | Roulette Logic :===");
 
 
 //===: Variables
+// Elementos HTML
 const tokens = document.querySelectorAll('.tokenCoin');
 const tokenNumbers = document.querySelectorAll('.token');
-const arrTokens = [['1'], ['5'], ['10'], ['25'], ['50'], ['100']];
-const arrTweaks = [['34', '1st 12'], ['35', '2nd 12'], ['36', '3rd 12'], ['37', '1 to 18'], ['38', '19 to 36'], ['39', 'Even'], ['40', 'Odd']];
-
 let actionSelected = document.getElementById("actionSelected");
 let btnActionSelected = document.getElementById("btnActionSelected");
 let rouletteCounterSec = document.getElementById("rouletteCounterSec");
 let rouletteCounter = document.getElementById("rouletteCounter");
 let rouletteSpin = document.getElementById("rouletteSpin");
-
-let tempSelected = ['0', '0'];
-let currentToken, idx;
-actionSelected.innerHTML = `0, 0`;
 let pasedSelected = document.getElementById("playedActions1");
-
 let salaryCounter = document.getElementById("userSalary");
-let usrSalary = 1940.50;
-const szTokens = 6;
-
 let winnerValueCont = document.getElementById("winnerVal");
 let winnerWrapper = document.getElementById("winnerWrapper");
 
+// Valores y arreglos
+const arrTokens = [['1'], ['5'], ['10'], ['25'], ['50'], ['100']];
+const arrTweaks = [['34', '1st 12'], ['35', '2nd 12'], ['36', '3rd 12'], ['37', '1 to 18'], ['38', '19 to 36'], ['39', 'Even'], ['40', 'Odd']];
+let tempSelected = ['0', '0'];
+let currentToken, idx;
+let usrSalary = 1940.50;
+const szTokens = 6;
 let winnerValue = 0;
+
 
 //===: Functions
 calcBank();
+
+// Calcula las acciones jugadas
 
 function calcPlayed(){
     arrTokens.forEach(arrToken => {
@@ -37,47 +37,28 @@ function calcPlayed(){
     });
 };
 
+// Calcula el saldo del usuario
+
 function calcBank(){
-    getSalary(); // first fetch to salary.
+    getSalary(); // Obtiene el salario inicial.
 };
 
-function getRandomValue(){
-    const randomVal = Math.random();
-    winnerValue = Math.floor(randomVal * 36); // multiply for possibles values.
-    salaryCounter.innerHTML = usrSalary - winnerValue;
-    winnerWrapper.style.visibility = "visible";
-    winnerValueCont.innerHTML = winnerValue;
-};
+// Obtiene el salario del usuario
+
 function getSalary(){
     salaryCounter.innerHTML = usrSalary;
 };
 
-const counterLimit = 5;
-let timeRemaining = counterLimit;
-let timeRepeat = 5;
+// Boton de Spin
+
 let isClicked = false;
 rouletteSpin.addEventListener('click', function onclick(event){
-    event.preventDefault();
-    isClicked = true;
-    if(isClicked){
-        const timer = setInterval(() => {
-            rouletteSpin.disabled = true;
-            timeRemaining--;
-            if(timeRemaining <= 0){
-                console.log("Tiempo terminado.");
-                calcBank();
-                calcPlayed();
-                clearInterval(timer);
-            }else {
-                rouletteCounterSec.innerHTML = timeRemaining;
-            };
-        }, 1000);
-    };
-    isClicked = false;
+    const randomNumber = Math.floor(Math.random() * 34); // Generar número aleatorio entre 0 y 99
+    console.log("Número aleatorio:", randomNumber);
 });
 
 
-//===: Events
+//Selecciona el numero
 for(let i=0; i<tokenNumbers.length; i++){
     tokenNumbers[i].addEventListener('click', function onclick(e){
         console.log(tokenNumbers[i].textContent);
@@ -85,6 +66,8 @@ for(let i=0; i<tokenNumbers.length; i++){
         actionSelected.innerHTML = `${tempSelected[0]}, ${tempSelected[1]}`;
     });
 };
+
+//Evento de clic en los tokens
 
 for(const token of tokens){
     token.disabled = true;
@@ -94,9 +77,11 @@ for(const token of tokens){
         currentToken = ((token.id).substring(5)).toString();
         tempSelected[0] = currentToken;
         actionSelected.innerHTML = `${tempSelected[0]}, ${tempSelected[1]}`;
-        // const pos = arrTokens.findIndex((subArr) => subArr[0] == currentToken); // return pos[0] of selected item
+        const pos = arrTokens.findIndex((subArr) => subArr[0] == currentToken); // return pos[0] of selected item
     });
 };
+
+// Evento de clic en el botón de acción seleccionada
 
 btnActionSelected.addEventListener('click', function onclick(e) {
     console.log("CLICKED", tempSelected[0]);
